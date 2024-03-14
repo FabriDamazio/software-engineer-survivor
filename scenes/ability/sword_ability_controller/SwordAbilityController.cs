bbf1c8c96bc26eef4f67721d8b212eb49edae66d
FabriDamazio <fabridamazio@gmail.com>
2024-03-13 21:04:54 -0300

    Spawn sword targeting the closest enemy

--- scenes/ability/sword_ability_controller/SwordAbilityController.cs
+++ scenes/ability/sword_ability_controller/SwordAbilityController.cs
@@ -1,8 +1,14 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 public partial class SwordAbilityController : Node
 {
+	[Export]
+	private float MaxRange = 150;
+
 	[Export]
 	public PackedScene SwordAbility;
 
@@ -14,10 +20,23 @@ public partial class SwordAbilityController : Node
 	private void OnTimerTimeout()
 	{
 		var player = GetTree().GetFirstNodeInGroup("player") as Node2D;
-		if(player == null) return;
+		if(player is null) return;
+
+		var enemies = GetTree().GetNodesInGroup("enemy").OfType<Node2D>();
+		var closeEnemies = enemies.Where(x => 
+			x.GlobalPosition.DistanceSquaredTo(player.GlobalPosition) < Math.Pow(MaxRange, 2)
+		).ToList();
+
+		if(closeEnemies.Count == 0) return;
+
+		closeEnemies.Sort((a,b) =>
+			a.GlobalPosition.DistanceSquaredTo(player.GlobalPosition).CompareTo(
+				b.GlobalPosition.DistanceSquaredTo(player.GlobalPosition)
+			)
+		);
 		
 		var sword_instance = SwordAbility.Instantiate() as Node2D;
 		player.GetParent().AddChild(sword_instance);
-		sword_instance.GlobalPosition = player.GlobalPosition;
+		sword_instance.GlobalPosition = closeEnemies.FirstOrDefault().GlobalPosition;
 	}
 }
