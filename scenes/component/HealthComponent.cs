2af7b4418c798304bfe784ba284e1fb5fd0af91d
FabriDamazio <fabridamazio@gmail.com>
2024-04-10 20:05:37 -0300

    Setup the basics of player damage

--- scenes/component/HealthComponent.cs
+++ scenes/component/HealthComponent.cs
@@ -9,23 +9,23 @@ public partial class HealthComponent : Node
 	[Signal]
 	public delegate void DiedEventHandler();
 
-	private float _currentHealth;
+	public float CurrentHealth { get; set; }
 
 	public override void _Ready()
 	{
-		_currentHealth = MaxHealth;
+		CurrentHealth = MaxHealth;
 	}
 
 	public void Damage(float damage)
 	{
-		_currentHealth = Math.Max(_currentHealth - damage, 0);
+		CurrentHealth = Math.Max(CurrentHealth - damage, 0);
 		Callable callable = new Callable(this, nameof(CheckDeath));
 		callable.CallDeferred();
 	}
 
 	private void CheckDeath()
 	{
-		if (_currentHealth <= 0)
+		if (CurrentHealth <= 0)
 		{
 			EmitSignal(SignalName.Died);
 			Owner.QueueFree();
--- scenes/game_object/player/Player.cs
+++ scenes/game_object/player/Player.cs
@@ -5,8 +5,18 @@ public partial class Player : CharacterBody2D
 {
 	private const float MaxSpeed = 125;
 	private const float AcelerationSmoothing = 25;
-	
-	public override void _Process(double delta)
+	private int _numberCollidingBodies = 0;
+	private Timer _damageIntervalTimer;
+
+	public override void _Ready()
+	{
+		_damageIntervalTimer = GetNode<Timer>("DamageIntervalTimer");
+		_damageIntervalTimer.Timeout += OnDamageIntervalTimerTimeout;
+		GetNode<Area2D>("CollisionArea2D").BodyEntered += OnBodyEntered;
+		GetNode<Area2D>("CollisionArea2D").BodyExited += OnBodyExited;
+	}
+
+    public override void _Process(double delta)
 	{
 		Vector2 direction = GetMovementVector().Normalized();
 		Vector2 targetVelocity = direction * MaxSpeed;
@@ -22,4 +32,32 @@ public partial class Player : CharacterBody2D
 
 		return new Vector2(xMovement, yMovement);
 	}
+
+	private void CheckDealDamage()
+	{
+		if (_numberCollidingBodies == 0 || !_damageIntervalTimer.IsStopped()) return;
+				
+		GetNode<HealthComponent>("HealthComponent").Damage(1);
+		_damageIntervalTimer.Start();
+	}
+
+	
+    private void OnBodyEntered(Node2D otherBody)
+    {
+		_numberCollidingBodies++;
+		CheckDealDamage();
+    }
+
+	
+    private void OnBodyExited(Node2D body)
+    {
+        _numberCollidingBodies--;
+    }
+
+	
+    private void OnDamageIntervalTimerTimeout()
+    {
+        CheckDealDamage();
+    }
+
 }
